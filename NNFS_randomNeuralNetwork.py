6682374e0997cd9310e8cce9bad3c72e810e668b
Aku <aku.nivala@gmail.com>
2021-10-11 20:38:31 +0300

    added adam and RMSprop optimizers

--- NNFS_randomNeuralNetwork.py
+++ NNFS_randomNeuralNetwork.py
@@ -245,6 +245,111 @@ class AdaGrad:
     def post_update_params(self):
         self.iterations +=1
 
+class Optmizer_RMSprop:
+    def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, 
+                rho=0.9):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay =  decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.rho = rho
+
+    # Call once before any parameter updates
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * \
+                (1. / (1+self.decay * self.iterations))
+    
+    # Update parameters
+    def update_params(self, layer):
+        # If layer does not contain cache arrays,
+        # create them filled with zeros
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+        # Update cache with squared current gradients
+        layer.weight_cache = self.rho * layer.weight_cache + \
+                            (1 - self.rho) * layer.dweights**2
+        layer.bias_cache = self.rho * layer.bias_cache + \
+                            (1 - self.rho) * layer.dbiases**2
+        # Vanilla SGD parameter update + normalization
+        # with square rooted cache
+        layer.weights += -self.current_learning_rate * \
+                            layer.dweights / \
+                            (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * \
+                        layer.dbiases / \
+                        (np.sqrt(layer.bias_cache) + self.epsilon)  
+
+    def post_update_params(self):
+        self.iterations +=1
+
+class Optmizer_Adam:
+    def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, 
+                beta_1=0.9, beta_2=0.999):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay =  decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+
+    # Call once before any parameter updates
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * \
+                (1. / (1+self.decay * self.iterations))
+    
+    # Update parameters
+    def update_params(self, layer):
+        # If layer does not contain cache arrays,
+        # create them filled with zeros
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+            layer.bias_cache = np.zeros_like(layer.biases)
+            
+        # Update cache with squared current gradients
+        layer.weight_momentums = self.beta_1 * \
+                                layer.weight_momentums + \
+                                (1 - self.beta_1) * layer.dweights
+        layer.bias_momentums = self.beta_1 * \
+                                layer.bias_momentums + \
+                                (1 - self.beta_1) * layer.dbiases
+        # Get corrected momentum
+        # self.iteration is 0 at first pass
+        # and we need to start with 1 here
+        weight_momentums_corrected = layer.weight_momentums / \
+                                    (1 - self.beta_1 ** (self.iterations + 1))
+        bias_momentums_corrected = layer.bias_momentums / \
+                                    (1 - self.beta_1 ** (self.iterations + 1))
+        # Update cache with squared current gradients
+        layer.weight_cache = self.beta_2 * layer.weight_cache + \
+                            (1 - self.beta_2) * layer.dweights**2  
+        layer.bias_cache = self.beta_2 * layer.bias_cache + \
+                            (1 - self.beta_2) * layer.dbiases**2
+        # Get corrected cache
+        weight_cache_corrected = layer.weight_cache / \
+                                (1 - self.beta_2 ** (self.iterations + 1))
+        bias_cache_corrected = layer.bias_cache / \
+                                (1 - self.beta_2 ** (self.iterations + 1))
+        # Vanilla SGD parameter update + normalization
+        # with square rooted cache
+        layer.weights += -self.current_learning_rate * \
+                        weight_momentums_corrected / \
+                        (np.sqrt(weight_cache_corrected) +
+                        self.epsilon)
+        layer.biases += -self.current_learning_rate * \
+                        bias_momentums_corrected / \
+                        (np.sqrt(bias_cache_corrected) +
+                        self.epsilon)
+
+    def post_update_params(self):
+        self.iterations +=1
+
 X, y = spiral_data(samples=100, classes=3)
 
 # input data has x and y cordinate
@@ -253,11 +358,13 @@ dense2 = layer_Dense(64, 3)
 activation1 = Activation_ReLU()
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 # optimizer = Optimizer_SGD(learning_rate=1, decay=1e-3, momentum=0.9)
-optimizer = AdaGrad(learning_rate=1, decay=1e-4)
+# optimizer = AdaGrad(learning_rate=1, decay=1e-4)
+# optimizer = Optmizer_RMSprop(learning_rate=0.02,decay=1e-4, rho=0.999)
+optimizer = Optmizer_Adam(learning_rate=0.05,decay=5e-7)
 
 
 
-for epoch in range(30001):
+for epoch in range(10001):
     dense1.forward(X)
     activation1.forward(dense1.output)
     dense2.forward(activation1.output)
@@ -273,7 +380,7 @@ for epoch in range(30001):
         y = np.argmax(y, axis=1)
     accuracy = np.mean(predictions==y)
 
-    if not epoch % 1000:
+    if not epoch % 100:
         print(f'epoch: {epoch}, ' +
                 f'acc: {accuracy:.3f}, ' +
                 f'loss: {loss:.3f} ' +
